2827dcfd54c700ccc15cfb3c26b73b79a67c3b9d
Tan LiLing <liling.tan@rakuten.com>
2017-09-13 10:54:17 +0800

    added more example

--- cranium.sh
+++ cranium.sh
@@ -6,7 +6,13 @@ python barathrum.py "$@" > arguments.json
 # Initialize the arguments according.
 dagger=$(cat arguments.json | jq '.["dagger"]')
 greater_bash=$(cat arguments.json | jq '.["greater_bash"]')
+e=$(cat arguments.json | jq '.["empower_haste"]')
 
-# Et voila! 
+#while read -r name value; do
+#    declare "$name=$value"
+#done < <(jq -r 'to_entries[] | "\(.key) \(.value)"')
+
+# Et voila!
 echo $dagger
 echo $greater_bash
+echo $e
